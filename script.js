43c1fd9fc2536e96f560ad0328eeaa2be9282c56
ikari4 <michael.ragusa@me.com>
2025-03-15 17:28:52 -0400

    Chain operations added

--- script.js
+++ script.js
@@ -1,59 +1,106 @@
 const displayElement = document.querySelector(".screen");
-
-const keyId = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
-    "enter", "multiply", "divide", "add", "subtract", "decimal", "cls"];
-for (let i = 0; i < keyId.length; i++) {
-    const selectedKey = document.getElementById(keyId[i]);
-    selectedKey.addEventListener("click", displayKey);
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const operators = ["multiply", "divide", "add", "subtract"]
+for (let i = 0; i < numbers.length; i++) {
+    const selectedKey = document.getElementById(numbers[i]);
+    selectedKey.addEventListener("click", displayNumber);
 }
+for (let i = 0; i < operators.length; i++) {
+    const selectedKey = document.getElementById(operators[i]);
+    selectedKey.addEventListener("click", displayOperator);
+}
+const enterPress = document.getElementById("enter");
+enterPress.addEventListener("click", calculate);
+
+const clsPress = document.getElementById("cls");
+clsPress.addEventListener("click", clearScreen);
+
+const decPress = document.getElementById("decimal");
+decPress.addEventListener("click", displayDec);
 
 var firstNum = "";
 var secondNum = "";
+var result = "";
+var operator = "";
 var opPressed = false;
-let operator = "";
+var newNum = true;
 
-function displayKey(event) {
-    
-    const keyPress = document.createElement("div");
-    keyPress.textContent = event.target.textContent;
-    if (event.target.className === "button num") {
-        displayElement.appendChild(keyPress);
-        if (opPressed === false) {
-            firstNum += event.target.textContent;
-        } else {
-            secondNum += event.target.textContent;
-        }  
-    } else if (keyPress.textContent === "cls") {
-        displayElement.textContent = "";
-        firstNum = "";
-        secondNum = "";
-        operator = "";
-        opPressed = false;
-    } else if (event.target.className === "button op") { 
-        keyPress.textContent = " " + event.target.textContent + " ";
-        keyPress.style.whiteSpace = 'pre';
-        displayElement.appendChild(keyPress);
-        operator = keyPress.textContent;
-        opPressed = true;
+function clearScreen(event) {
+    displayElement.textContent = "";
+    firstNum = "";
+    secondNum = "";
+    result = "";
+    operator = "";
+    opPressed = false;
+    newNum = true;
+}
+
+function displayNumber(event) {
+    const numPress = document.createElement("div");
+    numPress.textContent = event.target.textContent;
+    displayElement.appendChild(numPress);
+    if (opPressed === false) {
+        firstNum += event.target.textContent;
+        newNum = false;
     } else {
-        displayElement.textContent = "";
-        if (operator === " + ") {
-             keyPress.textContent = Number(firstNum) + Number(secondNum);
-             displayElement.appendChild(keyPress);
-        } else if (operator === " - ") {
-            keyPress.textContent = Number(firstNum) - Number(secondNum);
+        secondNum += event.target.textContent;
+        newNum = false;
+    }  
+}
+
+function displayOperator(event) {
+    const opPress = document.createElement("div");
+    operator = event.target.textContent;
+    opPress.textContent = " " + operator + " ";
+    opPress.style.whiteSpace = 'pre';
+    displayElement.appendChild(opPress);
+    opPressed = true;
+    newNum = true;
+}
+
+function displayDec(event) {
+    const decPress = document.createElement("div");
+    if (newNum === true) {
+        decPress.textContent = "0" + event.target.textContent;
+    } else {
+        decPress.textContent = event.target.textContent;
+    }
+    displayElement.appendChild(decPress);
+    if (opPressed === false) {
+        firstNum += event.target.textContent;
+        newNum = false;
+    } else {
+        secondNum += event.target.textContent;
+        newNum = false;
+    }  
+}
+
+function calculate() {
+    const keyPress = document.createElement("div");
+    displayElement.textContent = "";
+    if (operator === "+") {
+        result =  Number(firstNum) + Number(secondNum);
+        keyPress.textContent = Number(Math.round(result + 'e6') + 'e-6');
+        displayElement.appendChild(keyPress);
+    } else if (operator === "-") {
+        result =  Number(firstNum) - Number(secondNum);
+        keyPress.textContent = Number(Math.round(result + 'e6') + 'e-6');
+        displayElement.appendChild(keyPress);
+    } else if (operator === "/") {
+        if (Number(secondNum) === 0) {
+            keyPress.textContent = "Don't divide by 0! ";
             displayElement.appendChild(keyPress);
-        } else if (operator === " / ") {
-            if (Number(secondNum) === 0) {
-                keyPress.textContent = "Don't divide by 0!";
-                displayElement.appendChild(keyPress);
-            } else {
-                keyPress.textContent = Number(firstNum) / Number(secondNum);
-                displayElement.appendChild(keyPress);
-            }
-        } else {
-            keyPress.textContent = Number(firstNum) * Number(secondNum);
+        } else { 
+            result =  Number(firstNum) / Number(secondNum);
+            keyPress.textContent = Number(Math.round(result + 'e6') + 'e-6');
             displayElement.appendChild(keyPress);
         }
+    } else { //Multiply
+        result =  Number(firstNum) * Number(secondNum);
+        keyPress.textContent = Number(Math.round(result + 'e6') + 'e-6');
+        displayElement.appendChild(keyPress);
     }
+    firstNum = result;
+    secondNum = "";
+    operator = "";
 }
